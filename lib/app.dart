bdfe5e1e065fac3292bb4880758fce9d3df47668
log101 <frknerdm@gmail.com>
2021-11-18 12:47:01 +0300

    Implement dialogflow

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,37 @@
 import 'package:auth/features/auth/bloc/auth/auth_bloc.dart';
+import 'package:auth/features/auth/bloc/bloc.dart';
 import 'package:auth/features/auth/repository/auth_repository.dart';
+import 'package:auth/features/auth/repository/chatbot_repository.dart';
 import 'package:auth/features/auth/view/initializing_page.dart';
+import 'package:dialog_flowtter/dialog_flowtter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AuthApp extends StatelessWidget {
-  const AuthApp({Key? key}) : super(key: key);
+  AuthApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          AuthBloc(authRepository: AuthRepository(FirebaseAuth.instance)),
+    late DialogFlowtter dialogFlowtter;
+    DialogFlowtter.fromFile(
+            path: "assets/owl-app-dialogflow.json", sessionId: "12345678")
+        .then((value) => dialogFlowtter = value);
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              AuthBloc(authRepository: AuthRepository(FirebaseAuth.instance)),
+        ),
+        BlocProvider(
+          create: (context) {
+            return ChatbotCubit(
+                chatbotRepository: ChatbotRepository(dialogFlowtter));
+          },
+        ),
+      ],
       child: const InitializingPage(),
     );
   }
